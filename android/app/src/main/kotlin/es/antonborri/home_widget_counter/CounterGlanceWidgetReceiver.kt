b54ee9235de397403fef302799e8a908d661fdbf
Sanjide <sanjidmsnjdp@gmail.com>
2026-01-17 07:34:51 +0530

    Delete android/app/src/main/kotlin/es/antonborri/home_widget_counter/CounterGlanceWidgetReceiver.kt

--- android/app/src/main/kotlin/es/antonborri/home_widget_counter/CounterGlanceWidgetReceiver.kt
+++
@@ -1,7 +0,0 @@
-package es.antonborri.home_widget_counter
-
-import HomeWidgetGlanceWidgetReceiver
-
-class CounterGlanceWidgetReceiver : HomeWidgetGlanceWidgetReceiver<CounterGlanceWidget>() {
-    override val glanceAppWidget = CounterGlanceWidget()
-}

ce4f6b08437696325a0bd004b16dd58cd217b89e
Sanjide <sanjidmsnjdp@gmail.com>
2026-01-20 07:30:36 -0500

    Create firebase_config.dart

---
+++ lib/firebase_config.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: "AIzaSyCOT73k7YWxlh0qYFYGKa1W_NW29LjwsgQ",
+    appId: "1:476270819694:web:2689cf709656cfde1d697f",
+    messagingSenderId: "476270819694",
+    projectId: "fest-21d67",
+    storageBucket: "fest-21d67.firebasestorage.app",
+    authDomain: "fest-21d67.firebaseapp.com",
+    measurementId: "G-93HHHL4H2P",
+  );
+}
